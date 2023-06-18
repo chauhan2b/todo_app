e5c2c41ddcae29843aec4e512d9cb12bdb41bc0d
Bharat Chauhan <allmight524@gmail.com>
2023-06-18 16:16:56 +0530

    add persistance

--- lib/models/todo.dart
+++ lib/models/todo.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 class Todo {
   final String title;
@@ -25,4 +27,27 @@ class Todo {
       completed: completed ?? this.completed,
     );
   }
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'title': title,
+      'id': id,
+      'time': time.millisecondsSinceEpoch,
+      'completed': completed,
+    };
+  }
+
+  factory Todo.fromMap(Map<String, dynamic> map) {
+    return Todo(
+      title: map['title'] as String,
+      id: map['id'] as String,
+      time: DateTime.fromMillisecondsSinceEpoch(map['time'] as int),
+      completed: map['completed'] as bool,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory Todo.fromJson(String source) =>
+      Todo.fromMap(json.decode(source) as Map<String, dynamic>);
 }
--- lib/respositories/todo_repository.dart
+++ lib/respositories/todo_repository.dart
@@ -1,10 +1,37 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:convert';
+
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import '../models/todo.dart';
 
 class TodoRepository extends StateNotifier<List<Todo>> {
-  TodoRepository() : super([]);
+  TodoRepository() : super([]) {
+    loadTodos();
+  }
+
+  Future<void> loadTodos() async {
+    final prefs = await SharedPreferences.getInstance();
+    final encodedTodos = prefs.getString('todos');
+    if (encodedTodos != null) {
+      final decodedTodos = jsonDecode(encodedTodos) as List<dynamic>;
+      state = decodedTodos.map((json) => Todo.fromJson(json)).toList();
+    }
+  }
+
+  Future<void> saveTodos(List<Todo> todos) async {
+    final prefs = await SharedPreferences.getInstance();
+    final encodedTodos =
+        jsonEncode(todos.map((todo) => todo.toJson()).toList());
+    await prefs.setString('todos', encodedTodos);
+  }
+
+  @override
+  set state(List<Todo> newState) {
+    super.state = newState;
+    saveTodos(newState);
+  }
 
   void addTodo(Todo todo) {
     state = [todo, ...state];
