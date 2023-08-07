7acf84cd4006f11f927e7cb2e9450931e244d8b6
Bharat Chauhan <allmight524@gmail.com>
2023-08-07 19:32:32 +0530

    update icon

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:dynamic_color/dynamic_color.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:todo_app/routing/go_router.dart';
